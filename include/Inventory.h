db37a979817b14fe30635060ec5ca7d96350b0b8
dimon <dimon@DIMON>
2024-09-16 23:02:26 +0300

    tedxs

--- include/Inventory.h
+++ include/Inventory.h
@@ -3,18 +3,17 @@
 #include <memory>
 #include <SFML/Graphics.hpp>
 
-class Heroes;
-class Tile;
-class Inventory{
-public:
-	Inventory();
-	~Inventory() = default;
-	void add(std::unique_ptr<Heroes> item);
-	void remove(std::unique_ptr<Heroes> item);
-	void draw(sf::RenderWindow& window);
-	void update();
-	void processEvents(sf::RenderWindow& window);
-
-private:
-	std::vector<std::pair<Tile , std::unique_ptr<Heroes>>> m_items;
-};
+//class Heroes;
+//class Inventory{
+//public:
+//	Inventory();
+//	~Inventory() = default;
+//	void add(std::unique_ptr<Heroes> item);
+//	void remove(std::unique_ptr<Heroes> item);
+//	void draw(sf::RenderWindow& window);
+//	void update();
+//	void processEvents(sf::RenderWindow& window);
+//
+//private:
+//	std::vector<std::unique_ptr<Heroes>> m_items;
+//};
---
+++ include/InventoryUI.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+
+class InventoryUI {
+
+	
+private:
+	std::vector<sf::Sprite> m_slots;
+};
